81c59f413361c858bde3adcd2b62006588b631ef
Alejandro <alexorogom@gmail.com>
2023-06-20 08:12:10 -0600

    comment

--- Main.py
+++ Main.py
@@ -4,13 +4,15 @@ Created on Mon Jun  5 11:07:01 2023
 
 @author: Gregorio Alejandro Oropeza Gomez
 """
-import costos
+import costos as co
+import ONEfolio as single
 
 try:
-    NLCAD, NLCD = costos.loadData()
+    #Multiple folio data
+    NLCAD, NLCD = co.loadData()
+    folioDB = co.orderByFolio(NLCAD, NLCD)
+    co.calAndPrintAllTimes(folioDB, NLCAD)
+    #single folio data
     #folioData = searchFolio(NLCAD, NLCD) buscar un solo folio
-    folioDB = costos.orderByFolio(NLCAD, NLCD)
-    costos.calAndPrintAllTimes(folioDB, NLCAD)
-
 except(KeyboardInterrupt()):
     print("error, cierre del programa por usuario, vuelva a correr el programa")
